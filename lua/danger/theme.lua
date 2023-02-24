1c0d72daa15d692b776e1472446f9887584c87ae
Igor Guerrero <igorgue@protonmail.com>
2023-02-24 17:06:52 -0600

    feat(treesitter): add all treesitter highlights

--- lua/danger/theme.lua
+++ lua/danger/theme.lua
@@ -48,8 +48,8 @@ function M.load(opts, colors)
 	hi("VertSplit", { fg = colors.keyword2, bg = colors.bg })
 	hi("StatusLine", { fg = colors.fg4, bg = colors.bg })
 	hi("StatusLineNC", { fg = colors.bg, bg = colors.fg2, bold = true })
-	hi("Pmenu", { fg = colors.fg2, bg = colors.bg2 })
-	hi("PmenuSel", { fg = colors.type, bg = colors.bg4, bold = true })
+	hi("Pmenu", { fg = colors.fg2, bg = colors.bg2, blend = 5 })
+	hi("PmenuSel", { fg = colors.type, bg = colors.bg4, bold = true, blend = 0 })
 	hi("Directory", { fg = colors.const })
 	hi("Folded", { fg = colors.fg4, bg = colors.bg })
 	hi("WildMenu", { fg = colors.str, bg = colors.bg })
@@ -258,293 +258,80 @@ function M.load(opts, colors)
 	hi("MiniIndentscopeSymbol", { fg = colors.type, bold = true })
 
 	-- treesitter
-	hi("@function", { fg = colors.keyword2 })
-	hi("@variable", { fg = colors.fg })
+	hi("@annotation", { link = "PreProc" })
+	hi("@attribute", { link = "PreProc" })
+	hi("@boolean", { link = "Boolean" })
+	hi("@character", { link = "Character" })
+	hi("@character.special", { link = "SpecialChar" })
+	hi("@comment", { link = "Comment" })
+	hi("@conditional", { fg = colors.keyword2, bold = true })
+	hi("@constant.builtin", { link = "Special" })
+	hi("@constant", { link = "Constant" })
+	hi("@constant.macro", { link = "Define" })
+	hi("@constructor", { link = "Special" })
+	hi("@debug", { link = "Debug" })
+	hi("@define", { link = "Define" })
+	hi("@delimiter", { link = "Delimiter" })
+	hi("@exception", { fg = colors.keyword2, bold = true })
+	hi("@exception", { link = "Exception" })
 	hi("@field", { fg = colors.fg2 })
+	hi("@field", { link = "Identifier" })
+	hi("@float", { link = "Float" })
 	hi("@function.builtin", { fg = colors.keyword2 })
+	hi("@function.call", { link = "@function" })
+	hi("@function", { fg = colors.keyword2 })
+	hi("@function.macro", { link = "Macro" })
+	hi("@include", { link = "Include" })
 	hi("@keyword", { fg = colors.keyword2, bold = true })
 	hi("@keyword.function", { fg = colors.keyword2, bold = true, italic = true })
+	hi("@keyword.operator", { link = "Operator" })
+	hi("@keyword.return", { link = "Keyword" })
+	hi("@label", { link = "Label" })
+	hi("@method.call", { link = "@method" })
+	hi("@method", { link = "Function" })
+	hi("@namespace", { link = "Include" })
+	hi("@number", { link = "Number" })
+	hi("@operator", { link = "Operator" })
+	hi("@parameter", { link = "Identifier" })
+	hi("@parameter.reference", { link = "@parameter" })
+	hi("@preproc", { link = "PreProc" })
+	hi("@property", { link = "Identifier" })
+	hi("@punctuation.bracket", { link = "Delimiter" })
+	hi("@punctuation.delimiter", { link = "Delimiter" })
+	hi("@punctuation.special", { link = "Delimiter" })
 	hi("@repeat", { fg = colors.keyword2, bold = true })
-	hi("@conditional", { fg = colors.keyword2, bold = true })
-	hi("@exception", { fg = colors.keyword2, bold = true })
-	--   ["@annotation"] = {
-	--   default = true,
-	--   link = "PreProc",
-	-- },
-	-- ["@attribute"] = {
-	--   default = true,
-	--   link = "PreProc",
-	-- },
-	-- ["@boolean"] = {
-	--   default = true,
-	--   link = "Boolean",
-	-- },
-	-- ["@character"] = {
-	--   default = true,
-	--   link = "Character",
-	-- },
-	-- ["@character.special"] = {
-	--   default = true,
-	--   link = "SpecialChar",
-	-- },
-	-- ["@comment"] = {
-	--   default = true,
-	--   link = "Comment",
-	-- },
-	-- ["@conditional"] = {
-	--   default = true,
-	--   link = "Conditional",
-	-- },
-	-- ["@constant"] = {
-	--   default = true,
-	--   link = "Constant",
-	-- },
-	-- ["@constant.builtin"] = {
-	--   default = true,
-	--   link = "Special",
-	-- },
-	-- ["@constant.macro"] = {
-	--   default = true,
-	--   link = "Define",
-	-- },
-	-- ["@constructor"] = {
-	--   default = true,
-	--   link = "Special",
-	-- },
-	-- ["@debug"] = {
-	--   default = true,
-	--   link = "Debug",
-	-- },
-	-- ["@define"] = {
-	--   default = true,
-	--   link = "Define",
-	-- },
-	-- ["@exception"] = {
-	--   default = true,
-	--   link = "Exception",
-	-- },
-	-- ["@field"] = {
-	--   default = true,
-	--   link = "Identifier",
-	-- },
-	-- ["@float"] = {
-	--   default = true,
-	--   link = "Float",
-	-- },
-	-- ["@function"] = {
-	--   default = true,
-	--   link = "Function",
-	-- },
-	-- ["@function.builtin"] = {
-	--   default = true,
-	--   link = "Special",
-	-- },
-	-- ["@function.call"] = {
-	--   default = true,
-	--   link = "@function",
-	-- },
-	-- ["@function.macro"] = {
-	--   default = true,
-	--   link = "Macro",
-	-- },
-	-- ["@include"] = {
-	--   default = true,
-	--   link = "Include",
-	-- },
-	-- ["@keyword"] = {
-	--   default = true,
-	--   link = "Keyword",
-	-- },
-	-- ["@keyword.function"] = {
-	--   default = true,
-	--   link = "Keyword",
-	-- },
-	-- ["@keyword.operator"] = {
-	--   default = true,
-	--   link = "@operator",
-	-- },
-	-- ["@keyword.return"] = {
-	--   default = true,
-	--   link = "@keyword",
-	-- },
-	-- ["@label"] = {
-	--   default = true,
-	--   link = "Label",
-	-- },
-	-- ["@method"] = {
-	--   default = true,
-	--   link = "Function",
-	-- },
-	-- ["@method.call"] = {
-	--   default = true,
-	--   link = "@method",
-	-- },
-	-- ["@namespace"] = {
-	--   default = true,
-	--   link = "Include",
-	-- },
-	-- ["@none"] = {
-	--   default = true,
-	-- },
-	-- ["@number"] = {
-	--   default = true,
-	--   link = "Number",
-	-- },
-	-- ["@operator"] = {
-	--   default = true,
-	--   link = "Operator",
-	-- },
-	-- ["@parameter"] = {
-	--   default = true,
-	--   link = "Identifier",
-	-- },
-	-- ["@parameter.reference"] = {
-	--   default = true,
-	--   link = "@parameter",
-	-- },
-	-- ["@preproc"] = {
-	--   default = true,
-	--   link = "PreProc",
-	-- },
-	-- ["@property"] = {
-	--   default = true,
-	--   link = "Identifier",
-	-- },
-	-- ["@punctuation.bracket"] = {
-	--   default = true,
-	--   link = "Delimiter",
-	-- },
-	-- ["@punctuation.delimiter"] = {
-	--   default = true,
-	--   link = "Delimiter",
-	-- },
-	-- ["@punctuation.special"] = {
-	--   default = true,
-	--   link = "Delimiter",
-	-- },
-	-- ["@repeat"] = {
-	--   default = true,
-	--   link = "Repeat",
-	-- },
-	-- ["@storageclass"] = {
-	--   default = true,
-	--   link = "StorageClass",
-	-- },
-	-- ["@string"] = {
-	--   default = true,
-	--   link = "String",
-	-- },
-	-- ["@string.escape"] = {
-	--   ["@string.escape"] = {
-	--   default = true,
-	--   link = "SpecialChar",
-	-- },
-	-- ["@string.regex"] = {
-	--   default = true,
-	--   link = "String",
-	-- },
-	-- ["@string.special"] = {
-	--   default = true,
-	--   link = "SpecialChar",
-	-- },
-	-- ["@symbol"] = {
-	--   default = true,
-	--   link = "Identifier",
-	-- },
-	-- ["@tag"] = {
-	--   default = true,
-	--   link = "Label",
-	-- },
-	-- ["@tag.attribute"] = {
-	--   default = true,
-	--   link = "@property",
-	-- },
-	-- ["@tag.delimiter"] = {
-	--   default = true,
-	--   link = "Delimiter",
-	-- },
-	-- ["@text"] = {
-	--   default = true,
-	--   link = "@none",
-	-- },
-	-- ["@text.danger"] = {
-	--   default = true,
-	--   link = "WarningMsg",
-	-- },
-	-- ["@text.emphasis"] = {
-	--   default = true,
-	--   italic = true,
-	-- },
-	-- ["@text.environment"] = {
-	--   default = true,
-	--   link = "Macro",
-	-- },
-	-- ["@text.environment.name"] = {
-	--   default = true,
-	--   link = "Type",
-	-- },
-	-- ["@text.literal"] = {
-	--   default = true,
-	--   link = "String",
-	-- },
-	-- ["@text.math"] = {
-	--   default = true,
-	--   link = "Special",
-	-- },
-	-- ["@text.note"] = {
-	--   default = true,
-	--   link = "SpecialComment",
-	-- },
-	-- ["@text.reference"] = {
-	--   default = true,
-	--   link = "Constant",
-	-- },
-	-- ["@text.strike"] = {
-	--   strikethrough = true,
-	-- },
-	-- ["@text.strong"] = {
-	--   bold = true,
-	--   default = true,
-	-- },
-	-- ["@text.title"] = {
-	--   default = true,
-	--   link = "Title",
-	-- },
-	-- ["@text.underline"] = {
-	--   underline = true,
-	-- },
-	-- ["@text.uri"] = {
-	--   default = true,
-	--   link = "Underlined",
-	-- },
-	-- ["@text.warning"] = {
-	--   default = true,
-	--   link = "Todo",
-	-- },
-	-- ["@text.todo"] = {
-	--   default = true,
-	--   link = "Todo",
-	-- },
-	-- ["@type"] = {
-	--   default = true,
-	--   link = "Type",
-	-- },
-	-- ["@type.builtin"] = {
-	--   default = true,
-	--   link = "Type",
-	-- },
-	-- ["@type.definition"] = {
-	--   default = true,
-	--   link = "Typedef",
-	-- },
-	-- ["@type.qualifier"] = {
-	--   default = true,
-	--   link = "Type",
-	-- },
-	-- ["@variable.builtin"] = {
-	--   default = true,
-	--   link = "Special",
-	-- },
+	hi("@repeat", { link = "Repeat" })
+	hi("@storageclass", { link = "StorageClass" })
+	hi("@string.escape", { link = "SpecialChar" })
+	hi("@string", { link = "String" })
+	hi("@string.regex", { link = "String" })
+	hi("@string.special", { link = "SpecialChar" })
+	hi("@symbol", { link = "Identifier" })
+	hi("@tag.attribute", { link = "@property" })
+	hi("@tag.delimiter", { link = "Delimiter" })
+	hi("@tag", { link = "Label" })
+	hi("@text.danger", { link = "WarningMsg" })
+	hi("@text.emphasis", { italic = true })
+	hi("@text.environment", { link = "Macro" })
+	hi("@text.environment.name", { link = "Macro" })
+	hi("@text", { link = "@none" })
+	hi("@text.literal", { link = "String" })
+	hi("@text.math", { link = "Special" })
+	hi("@text.note", { link = "SpecialComment" })
+	hi("@text.reference", { link = "Constant" })
+	hi("@text.strike", { strikethrough = true })
+	hi("@text.strong", { bold = true })
+	hi("@text.title", { link = "Title" })
+	hi("@text.todo", { link = "Todo" })
+	hi("@text.underline", { underline = true })
+	hi("@text.uri", { link = "Underlined" })
+	hi("@text.warning", { link = "Todo" })
+	hi("@type.builtin", { link = "Type" })
+	hi("@type.definition", { link = "Typedef" })
+	hi("@type", { link = "Type" })
+	hi("@type.qualifier", { link = "Type" })
+	hi("@variable.builtin", { link = "Special" })
+	hi("@variable", { fg = colors.fg })
 
 	-- markid plugin
 	hi("markid1", { fg = colors.extra1 })
